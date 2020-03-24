52679961a6917eeacf43f087e51fb2e730655e08
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-24 19:56:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,11 @@ def run_guessing_game
   secret_number = rand(6)+1
   puts "Guess a my number between 1 and 6!"
   user_guess = gets.chomp
-  
+  if user_guess == secret_number
+    puts ""
+  elsif user_guess == "exit"
+    puts "Goodbye!"
+  end 
   
 end 
 
